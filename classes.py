d0459305fb8ee72180bdd292627ec31cd99222d9
rishi <rishivardhan.prodduturi@gmail.com>
2026-04-05 15:42:56 -0700

    test commit

---
+++ classes.py
@@ -0,0 +1,15 @@
+#shopping_cart.add()
+#shopping_cart.remove()
+#shopping_cart.pop()
+
+#point.draw()
+#point.move()
+#point.get_distance(1,2)
+
+x = 1
+print(type(x))
+
+#Class is blueprint for creating new objects
+# object is an istnace of a class
+# class: Humans
+# objects: john, mary, jack
